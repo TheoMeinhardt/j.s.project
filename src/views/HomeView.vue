da6ea494a7cc417cdc3dd6c7aeb1b87163489e6f
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-06 01:28:35 +0100

    refactor HomeView layout; integrate HomeBanner and improve responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,42 +1,26 @@
 <template>
-  <div class="banner row window-height content-center">
-    <div class="col-5">
-      <q-img src="/images/Logo.png" class="col q-ml-xl logoBubble" width="40%"></q-img>
+  <HomeBanner />
 
-      <div class="text-weight-bold text-white text-h2 q-ml-xl q-mt-xl">
-        <span>
-          TRAINING & ERNÄHRUNG <br />
-          WORKSHOPS <br />
-          VORTRÄGE
-        </span>
-      </div>
-    </div>
-
-    <div class="col-7 text-center">
-      <q-img src="https://via.assets.so/img.jpg?w=500&h=500&tc=blue&bg=#121212" class="imageBubble" width="70%"></q-img>
-    </div>
+  <!-- Big screens: -->
+  <div class="gt-sm q-pa-xl">
+    <IntroParagraph />
+    <Divider />
+    <AboutUs />
   </div>
 
-  <div class="q-pa-xl">
+  <!-- Small screens: -->
+  <div class="lt-md q-px-lg q-py-md">
     <IntroParagraph />
+    <Divider />
+    <AboutUs />
   </div>
 </template>
 
 <script setup lang="ts">
+import HomeBanner from '@/components/HomeBanner.vue'
 import IntroParagraph from '@/components/IntroParagraph.vue'
+import AboutUs from '@/components/AboutUs.vue'
+import Divider from '@/components/ParagraphDivider.vue'
 </script>
 
-<style lang="scss" scoped>
-.banner {
-  background-color: $darkGreen;
-}
-
-.logoBubble {
-  border-radius: 9999px;
-  background-image: linear-gradient(#2b793e, #43a15c, #51c975);
-}
-
-.imageBubble {
-  border-radius: 9999px;
-}
-</style>
+<style lang="scss" scoped></style>
