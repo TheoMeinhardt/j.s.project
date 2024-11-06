9a10a768dbfbfa26ee967dd4c862ce3ca309f2f8
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-06 01:27:59 +0100

    refactor IntroParagraph component layout; add image and improve text formatting

--- src/components/IntroParagraph.vue
+++ src/components/IntroParagraph.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="text-body1">
-    <p>
-      Aus unserer langjährigen Erfahrung in den Bereichen Kraftsport, Gruppenfitness und Ernährungscoaching haben wir <span class="text-bold">jsproject</span> ins Leben gerufen. Unsere Vorträge, Seminare und Workshops zum Thema Krafttraining und Ernährung vereinen Leidenschaft mit Fachwissen und bieten individuelle Betreuung und Beratung – keine Frage bleibt unbeantwortet. Wir vermitteln fundiertes Wissen über die Grundlagen des Krafttrainings, die richtige Technik und Trainingsmethoden, um
-      individuelle Ziele zu erreichen. Besonders am Herzen liegt uns die Bedeutung des Krafttrainings und Ernährunbng für die Gesundheit und Fitness in jeder Altersgruppe. In unseren Workshops stehen praktische Übungen und Demonstrationen im Mittelpunkt. Sie helfen, theoretische Fakten verständlicher zu machen, und dabei berücksichtigen wir das Zeitmanagement und den Alltag jedes Teilnehmers. Da jeder Mensch einzigartig ist, ist die Individualisierung von Training und Ernährung entscheidend, um
-      Fortschritte zu erzielen. Motivation und Erfolge stärken dabei nicht nur den Körper, sondern auch das Selbstbewusstsein. Wir schulen den sicheren Umgang mit freien Gewichten und die individuelle Einstellung an Kraftgeräten, um Fehler zu vermeiden und Verletzungen vorzubeugen. Zudem schaffen wir einen Raum, in dem sich alle Teilnehmer austauschen, vernetzen und ihre Erfahrungen in das Seminar einbringen können.
-    </p>
-    <p>Wir freuen uns auf Euch!</p>
+  <div class="row flex-center wrap">
+    <div class="col-md-5 col-xs-12 q-mb-lg">
+      <q-img src="/images/8718.jpg"></q-img>
+    </div>
+
+    <div class="col-md-1 col-xs-12" />
+
+    <div class="text-body1 text-justify col-md-6 col-xs-12">
+      <p>
+        Aus unserer langjährigen Erfahrung in den Bereichen Kraftsport, Gruppenfitness und Ernährungscoaching haben wir <span class="text-bold">jsproject</span> ins Leben gerufen. Unsere Vorträge, Seminare und Workshops zum Thema Krafttraining und Ernährung vereinen Leidenschaft mit Fachwissen und bieten individuelle Betreuung und Beratung – keine Frage bleibt unbeantwortet. Wir vermitteln fundiertes Wissen über die Grundlagen des Krafttrainings, die richtige Technik und Trainingsmethoden, um
+        individuelle Ziele zu erreichen. Besonders am Herzen liegt uns die Bedeutung des Krafttrainings und Ernährunbng für die Gesundheit und Fitness in jeder Altersgruppe. In unseren Workshops stehen praktische Übungen und Demonstrationen im Mittelpunkt. Sie helfen, theoretische Fakten verständlicher zu machen, und dabei berücksichtigen wir das Zeitmanagement und den Alltag jedes Teilnehmers. Da jeder Mensch einzigartig ist, ist die Individualisierung von Training und Ernährung entscheidend,
+        um Fortschritte zu erzielen. Motivation und Erfolge stärken dabei nicht nur den Körper, sondern auch das Selbstbewusstsein. Wir schulen den sicheren Umgang mit freien Gewichten und die individuelle Einstellung an Kraftgeräten, um Fehler zu vermeiden und Verletzungen vorzubeugen. Zudem schaffen wir einen Raum, in dem sich alle Teilnehmer austauschen, vernetzen und ihre Erfahrungen in das Seminar einbringen können.
+      </p>
+      <p class="text-weight-bold">Wir freuen uns auf Euch!</p>
+    </div>
   </div>
 </template>
 
