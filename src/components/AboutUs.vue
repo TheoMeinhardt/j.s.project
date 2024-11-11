dec7d69a53a38e1772a4c0acc512001e3c54fab6
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-11 20:50:41 +0100

    refactor AboutUs component layout; enhance structure and improve readability

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="text-center">
-    <span class="text-h4">Wir über uns</span>
-  </div>
-
-  <div class="row items-center q-mt-lg">
-    <q-img src="/images/8650.jpg" class="portraitImage col-sm-2 col-xs-12"></q-img>
-
-    <div class="col-sm-1"></div>
-
-    <div class="col-sm-9 text-justify">
-      <!-- For margin top on flexbox wrap -->
-      <div class="lt-sm q-mt-lg"></div>
-
-      <p class="text-h5">Sandra Schelivsky</p>
-      <span class="text-body1">
-        Mein Name ist Sandra Schelivsky, ich bin 1971 geboren, Mutter von drei Kindern und selbstständige Fitness- und Ernährungstrainerin in Brunn am Gebirge bei Wien. Die Leidenschaft für den Kraftsport begleitet mich seit über 30 Jahren. Immer bestrebt meine Erfahrungen zu erweitern, bestritt ich 2019 drei internationale Wettkämpfe im Natural Bodybuilding. Als diplomierter Strength Coach begleite ich meine Kunden auf ihrem Weg, ihre persönlichen Ziele zu erreichen. Meine Stärken sind
-        Zielstrebigkeit, Kreativität, Empathie und authentische Beratung für Menschen jeden Alters, Geschlechts und Sportlichkeitsniveaus.
-      </span>
+  <div>
+    <div class="text-center">
+      <span class="text-h4">Wir über uns</span>
     </div>
-  </div>
 
-  <div class="row wrap items-center q-mt-xl">
-    <q-img src="/images/8671.jpg" class="portraitImage col-sm-2 col-xs-12"></q-img>
+    <div class="row items-center q-mt-lg">
+      <q-img src="/images/8650.jpg" class="portraitImage col-sm-2 col-xs-12"></q-img>
 
-    <div class="col-sm-1"></div>
+      <div class="col-sm-1"></div>
 
-    <div class="col-sm-9 text-justify">
-      <!-- For margin top on flexbox wrap -->
-      <div class="lt-sm q-mt-lg"></div>
+      <div class="col-sm-9 text-justify">
+        <!-- For margin top on flexbox wrap -->
+        <div class="lt-sm q-mt-lg"></div>
 
-      <p class="text-h5">Jacqueline Schoppelt</p>
-      <span class="text-body1">
-        Ich bin Jacqueline Schoppelt, 1979 in Wien geboren und seit 2008 zuerst nebenberuflich und nun hauptberuflich im Bereich Groupfitness- und Personaltraining tätig. Ein großer Bereich ist hier nach wie vor die Groupfitness mit den Schwerpunkten Funktional Training wie DEEPWORK und Crosstraining sowie in den Bereichen Pilates, BODYART & Backhealth. Mit meiner Ausbildung bei Intelligent Strength sowie vielzähligen Weiterbildungen im Bereich Krafttraining fand ich meine persönliche
-        Leidenschaft! Seit 2020 bin ich Masterinstructor und Ausbildnerin in der BODYART HEALTH ACADEMY und die Reise geht mit JS Project weiter! Mein Motto: „Stay Strong“ und dies bedeutet deinen Körper in Form zu halten, fit und gesund zu bleiben und ihn gut zu versorgen!
-      </span>
+        <p class="text-h5">Sandra Schelivsky</p>
+        <span class="text-body1">
+          Mein Name ist Sandra Schelivsky, ich bin 1971 geboren, Mutter von drei Kindern und selbstständige Fitness- und Ernährungstrainerin in Brunn am Gebirge bei Wien. Die Leidenschaft für den Kraftsport begleitet mich seit über 30 Jahren. Immer bestrebt meine Erfahrungen zu erweitern, bestritt ich 2019 drei internationale Wettkämpfe im Natural Bodybuilding. Als diplomierter Strength Coach begleite ich meine Kunden auf ihrem Weg, ihre persönlichen Ziele zu erreichen. Meine Stärken sind
+          Zielstrebigkeit, Kreativität, Empathie und authentische Beratung für Menschen jeden Alters, Geschlechts und Sportlichkeitsniveaus.
+        </span>
+      </div>
+    </div>
+
+    <div class="row wrap items-center q-mt-xl">
+      <q-img src="/images/8671.jpg" class="portraitImage col-sm-2 col-xs-12"></q-img>
+
+      <div class="col-sm-1"></div>
+
+      <div class="col-sm-9 text-justify">
+        <!-- For margin top on flexbox wrap -->
+        <div class="lt-sm q-mt-lg"></div>
+
+        <p class="text-h5">Jacqueline Schoppelt</p>
+        <span class="text-body1">
+          Ich bin Jacqueline Schoppelt, 1979 in Wien geboren und seit 2008 zuerst nebenberuflich und nun hauptberuflich im Bereich Groupfitness- und Personaltraining tätig. Ein großer Bereich ist hier nach wie vor die Groupfitness mit den Schwerpunkten Funktional Training wie DEEPWORK und Crosstraining sowie in den Bereichen Pilates, BODYART & Backhealth. Mit meiner Ausbildung bei Intelligent Strength sowie vielzähligen Weiterbildungen im Bereich Krafttraining fand ich meine persönliche
+          Leidenschaft! Seit 2020 bin ich Masterinstructor und Ausbildnerin in der BODYART HEALTH ACADEMY und die Reise geht mit JS Project weiter! Mein Motto: „Stay Strong“ und dies bedeutet deinen Körper in Form zu halten, fit und gesund zu bleiben und ihn gut zu versorgen!
+        </span>
+      </div>
     </div>
   </div>
 </template>
