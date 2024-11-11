6c68692849f8753889a4a8184b171e9a908a2ccf
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-11 20:51:09 +0100

    add GaleryParagraph component; implement image carousel for gallery display

---
+++ src/components/GaleryParagraph.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="text-center">
+      <p class="text-h4">Galerie</p>
+    </div>
+
+    <div class="q-my-xl">
+      <q-carousel swipeable animated height="500px" v-model="slide" thumbnails infinite autoplay>
+        <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+        <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+        <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      </q-carousel>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const slide = ref(1)
+</script>
