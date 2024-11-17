8e506bbc866b1728e5b27075061b248874c84c53
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-17 23:42:22 +0100

    enhance WorkShops component layout and content for Privat- and Firmenkunden

--- src/components/WorkShops.vue
+++ src/components/WorkShops.vue
@@ -5,9 +5,263 @@
       <p class="text-subtitle1">Workshops zu ausgewählten Schwerpunkten im Bereich Training, Gesundheit und Ernährung für Privat- und Firmenkunden</p>
     </div>
 
-    <div class="row justify-center text-center text-white text-body1">
-      <q-btn class="button col-auto q-mx-md q-my-sm" to="privatkunden" size="lg" unelevated rounded label="Privatkunden" />
-      <q-btn class="button col-auto q-mx-md q-my-sm" to="firmenkunden" size="lg" unelevated rounded label="Firmenkunden" />
+    <div class="row justify-around text-center text-white text-body1"></div>
+
+    <div class="row justify-around wrap">
+      <div class="col-md-4 col-xs-12">
+        <div class="text-center q-my-xl">
+          <span class="listHeading text-white text-h6 q-pa-md">Privatkunden</span>
+        </div>
+
+        <!-- Big Screens -->
+        <div class="gt-sm workshopParagraph text-justify">
+          <span class="text-body2">Starte jetzt mit deinem persönlichen @home-workshop für dich und deine Freunde. Diese interaktiven Workshops eignen sich ideal für Menschen, die in einer kleinen Runde persönliche Unterstützung und praxisnahe Tipps erhalten möchten. Ein privater Workshop über Ernährung und Training bei dir zu Hause ist eine großartige Möglichkeit, gezielt auf individuelle Bedürfnisse einzugehen. Unsere Workshops vereinen Theorie, Praxis und persönliche Betreuung. </span>
+        </div>
+        <!-- Small Screens -->
+        <div class="lt-md q-mb-md text-justify">
+          <span class="text-body2">Starte jetzt mit deinem persönlichen @home-workshop für dich und deine Freunde. Diese interaktiven Workshops eignen sich ideal für Menschen, die in einer kleinen Runde persönliche Unterstützung und praxisnahe Tipps erhalten möchten. Ein privater Workshop über Ernährung und Training bei dir zu Hause ist eine großartige Möglichkeit, gezielt auf individuelle Bedürfnisse einzugehen. Unsere Workshops vereinen Theorie, Praxis und persönliche Betreuung. </span>
+        </div>
+
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="groups" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Teilnehmerzahl </q-item-label>
+              <q-item-label> max. <span class="text-weight-bold">5 Personen</span> </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="timer" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Dauer </q-item-label>
+              <q-item-label> ca. <span class="text-weight-bold">3 Stunden</span> </q-item-label>
+              <q-item-label caption> inklusive Zeit für Fragen und persönlichen Austausch. </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="euro" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>Preis</q-item-label>
+              <q-item-label>pro workshop <span class="text-weight-bold">€390,--</span></q-item-label>
+              <q-item-label caption>zzgl. Anreise ab 10km + ev. Parkgebühren</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="library_books" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Zusatzmaterial</q-item-label>
+              <q-item-label>inkl. <span class="text-weight-bold">Skripten</span> und diverses <span class="text-weight-bold">Anschauungsmaterial</span></q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="explore" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label><span class="text-weight-bold">Kompass</span> durch den <span class="text-weight-bold">Lebensmittel- und Diätdschungel</span></q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="sports_score" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Training – Was, Wie und Warum <span class="text-weight-bold">Zielsetzung, Fortschritt, Motivation</span></q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="favorite" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label><span class="text-weight-bold">Ü50</span> – Fit durch Krafttraining und Ernährung</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            ><q-item-section avatar>
+              <q-icon color="secondary" name="check_circle" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label><span class="text-weight-bold">Individuelle Themenauswahl und Zusammenstellung</span> gerne auf Anfrage</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+
+      <div class="col-md-4 col-xs-12">
+        <div class="text-center q-my-xl">
+          <span class="listHeading text-white text-h6 q-pa-md">Firmenkunden</span>
+        </div>
+
+        <!-- Big Screens -->
+        <div class="gt-sm workshopParagraph text-justify">
+          <span class="text-body2">Um im Berufsalltag fit und leistungsfähig zu bleiben, sind Ernährung und Training entscheidend Mögliche Themen, die individuell auf ihre Mitarbeiter und Anforderungen per Rücksprache abgestimmt werden. </span>
+        </div>
+        <!-- Small Screen -->
+        <div class="lt-md q-mb-md text-justify">
+          <span class="text-body2">Um im Berufsalltag fit und leistungsfähig zu bleiben, sind Ernährung und Training entscheidend Mögliche Themen, die individuell auf ihre Mitarbeiter und Anforderungen per Rücksprache abgestimmt werden. </span>
+        </div>
+
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="groups" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Teilnehmerzahl </q-item-label>
+              <q-item-label> max. <span class="text-weight-bold">5 Personen</span> </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="timer" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Dauer </q-item-label>
+              <q-item-label> ca. <span class="text-weight-bold">3 Stunden</span> </q-item-label>
+              <q-item-label caption> inklusive Zeit für Fragen und persönlichen Austausch. </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="euro" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>Preis</q-item-label>
+              <q-item-label>pro workshop <span class="text-weight-bold">€590,--</span></q-item-label>
+              <q-item-label caption>zzgl. Anreise ab 10km + ev. Parkgebühren</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="library_books" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Zusatzmaterial</q-item-label>
+              <q-item-label>inkl. <span class="text-weight-bold">Skripten</span> und diverses <span class="text-weight-bold">Anschauungsmaterial</span></q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Fit & Gesund im Job: Erfolgsfaktor Firmenfitness</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Mit Bewegung und Ernährung: So bleiben Sie fit im Job</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Resilient und Stark: Mit Firmenfitness gegen Stress</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            ><q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Gesund und Leistungsfähig: Fit im Job durch Firmenfitness</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            ><q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Stressfrei und Fit: Ihre Gesundheit im Fokus</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            ><q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Aktiv im Arbeitsalltag: Fit bleiben durch Firmenfitness</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            ><q-item-section avatar>
+              <q-icon color="secondary" name="arrow_forward" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Stark durch den Tag: Bewegung und Ernährung im Job</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+
+        <div class="q-my-lg">
+          <span class="text-h6">Vortäge</span>
+        </div>
+
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="timer" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Dauer </q-item-label>
+              <q-item-label> ca. <span class="text-weight-bold">90 Minuten</span> </q-item-label>
+              <q-item-label caption> inklusive Zeit für Fragen und persönlichen Austausch. </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="euro" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>Preis</q-item-label>
+              <q-item-label>pro workshop <span class="text-weight-bold">€250,--</span></q-item-label>
+              <q-item-label caption>zzgl. Anreise ab 10km + ev. Parkgebühren</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="secondary" name="library_books" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline> Zusatzmaterial</q-item-label>
+              <q-item-label>inkl. <span class="text-weight-bold">Skripten</span> und diverses <span class="text-weight-bold">Anschauungsmaterial</span></q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +269,12 @@
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-.button {
+.listHeading {
   background-image: linear-gradient(#2b793e, #43a15c, #51c975);
+  border-radius: 999px;
+}
+
+.workshopParagraph {
+  height: 27vh;
 }
 </style>
