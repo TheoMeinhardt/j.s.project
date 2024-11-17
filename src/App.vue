9fa225f9a9d53dcbabb41e73607826950948f70f
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-11-18 00:24:36 +0100

    Add Oswald font styles to App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,25 @@ import { RouterView } from 'vue-router'
 
 <style scoped></style>
 
-<style lang="css" module="colors">
+<style lang="scss">
+@font-face {
+  font-family: 'Oswald-Medium';
+  src: url('./assets/fonts/Oswald/Oswald-Medium.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Oswald-Regular';
+  src: url('./assets/fonts/Oswald/Oswald-Regular.ttf') format('truetype');
+}
+
+.text-h4 {
+  font-family: 'Oswald-Medium';
+}
+
+.text-h5 {
+  font-family: 'Oswald-Regular';
+}
+
 .darkGreenBG {
   background-color: #126e2a;
 }
