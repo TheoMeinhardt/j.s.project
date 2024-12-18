5dd4ccc3771c051e195e9d24e84484c5a7e45a18
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-12-18 01:06:15 +0100

    Update GaleryParagraph component to enhance carousel functionality and image sources

--- src/components/GaleryParagraph.vue
+++ src/components/GaleryParagraph.vue
@@ -4,12 +4,29 @@
       <p class="text-h4">Galerie</p>
     </div>
 
-    <div class="q-my-xl">
-      <q-carousel swipeable animated height="500px" v-model="slide" thumbnails infinite autoplay>
-        <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-        <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-        <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-        <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    <div class="q-my-xl gt-sm">
+      <q-carousel swipeable animated height="95vh" v-model="slide" thumbnails infinite autoplay>
+        <q-carousel-slide :name="1" img-src="/images/Galery01.jpg" />
+        <q-carousel-slide :name="2" img-src="/images/Galery02.jpg" />
+        <q-carousel-slide :name="3" img-src="/images/Galery03.jpg" />
+        <q-carousel-slide :name="4" img-src="/images/Galery04.jpg" />
+        <q-carousel-slide :name="5" img-src="/images/Galery05.jpg" />
+        <q-carousel-slide :name="6" img-src="/images/Galery06.jpg" />
+        <q-carousel-slide :name="7" img-src="/images/Galery07.jpg" />
+        <q-carousel-slide :name="8" img-src="/images/Galery08.jpg" />
+      </q-carousel>
+    </div>
+
+    <div class="q-my-xl lt-md">
+      <q-carousel swipeable animated height="35vh" v-model="slide" thumbnails infinite autoplay>
+        <q-carousel-slide :name="1" img-src="/images/Galery01.jpg" />
+        <q-carousel-slide :name="2" img-src="/images/Galery02.jpg" />
+        <q-carousel-slide :name="3" img-src="/images/Galery03.jpg" />
+        <q-carousel-slide :name="4" img-src="/images/Galery04.jpg" />
+        <q-carousel-slide :name="5" img-src="/images/Galery05.jpg" />
+        <q-carousel-slide :name="6" img-src="/images/Galery06.jpg" />
+        <q-carousel-slide :name="7" img-src="/images/Galery07.jpg" />
+        <q-carousel-slide :name="8" img-src="/images/Galery08.jpg" />
       </q-carousel>
     </div>
   </div>
