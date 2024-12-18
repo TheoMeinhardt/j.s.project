50171ce1dc9944d9708ca9f713eb145357e515d5
TheoMeinhardt <theo.meinhardt@batinfo.at>
2024-12-18 13:19:25 +0100

    Add detailed content to ImpressumView component

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,5 +1,38 @@
 <template>
-  <h1>Test</h1>
+  <div class="q-pa-xl">
+    <p class="text-h2">Impressum</p>
+    <p class="text-h5">Offenlegung nach § 25 Mediengesetz</p>
+
+    <div class="text-body1">
+      <p>Sandra Schelivsky<br />Römerweg 7/1/4<br />2345 Brunn am Gebirge</p>
+      <p>
+        <span class="text-bold">Rechtsform: </span>Kleinunternehmer<br />
+        <span class="text-bold">UID und UST: </span>befreit, nach § 6 Abs 1 Z 27 UstG. - Kleinunternehmerregelung <br />
+        <span class="text-bold">Telefon: </span> +43 699 11878758 <br />
+        <span class="text-bold">Email: </span><a href="mailto:office@jsproject.at">office@jsproject.at</a>
+      </p>
+
+      <div>
+        <br />
+        <p>
+          <b>EU-Streitschlichtung<br /></b>Angaben zur Online-Streitbeilegung: Verbraucher haben die Möglichkeit, Beschwerden an die OnlineStreitbeilegungsplattform der EU zu richten: <a href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&amp;lng=DE">https://ec.europa.eu/consumers/odr</a>. Sie können allfällige Beschwerde auch an die oben angegebene E-Mail-Adresse richten.
+        </p>
+        <p>HAFTUNGSAUSSCHLUSS<br />Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese Webseite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.</p>
+        <p>
+          HAFTUNG FÜR INHALTE DIESER WEBSEITE<br />Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Wir sind jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+        </p>
+        <p>
+          HAFTUNG FÜR LINKS AUF WEBSEITEN DRITTER<br />Unser Angebot enthält Links zu externen Websites. Auf den Inhalt dieser externen Webseiten haben wir keinerlei Einfluss. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
+          Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+        </p>
+        <p>
+          URHEBERRECHT<br />Die Betreiber dieser Webseite sind bemüht, stets die Urheberrechte anderer zu beachten bzw. auf selbst erstellte sowie lizenzfreie Werke zurückzugreifen. Die durch die Seitenbetreiber erstellten Inhalte und Werke auf dieser Webseite unterliegen dem Urheberrecht. Beiträge Dritter sind als solche gekennzeichnet. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
