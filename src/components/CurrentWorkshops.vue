e223494c8ee4d6058070b73fde49a5c8be60a1ae
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-03-12 00:25:55 +0100

    added dynamic current workshops
    Reads the data for current Workshops from a JSON file and dynamically
    displays all of them.

--- src/components/CurrentWorkshops.vue
+++ src/components/CurrentWorkshops.vue
@@ -4,10 +4,10 @@
       <span class="text-h4">Aktuelle Workshops</span>
     </div>
 
-    <div class="row justify-around reverse-wrap q-mt-xl">
+    <div v-for="(workshop, key) in workshops" class="row justify-around reverse-wrap q-mt-xl" :key=key>
       <div class="col-12 col-md-5">
-        <p class="text-h5">Krafttraining - deine Power Formel</p>
-        <span class="text-body1"> wie gezieltes Krafttraining zu mehr Energie, einem stärkeren Herzen, gesünderen Knochen und einer höheren Stressresistenz führt. Teilnehmende lernen, wie sie durch den richtigen Trainingsreiz, strukturierte Planung und konsequente Motivation ihre Muskulatur stärken, den Körper straffen und ihre Lebensqualität langfristig verbessern können. </span>
+        <p class="text-h5">{{ workshop.titel }}</p>
+        <span class="text-body1"> {{ workshop.text }} </span>
 
         <q-list bordered separator class="q-mt-lg">
           <q-item>
@@ -16,7 +16,8 @@
             </q-item-section>
             <q-item-section>
               <q-item-label overline> Wann </q-item-label>
-              <q-item-label> <span class="text-weight-bold">06.12.2025</span>, 11:30 - 13:00 </q-item-label>
+              <q-item-label> <span class="text-weight-bold">{{ format(workshop.begin, 'dd.MM.yyyy') }}</span>,
+                {{ format(workshop.begin, 'HH:mm') }} - {{ format(workshop.end, 'HH:mm') }} </q-item-label>
             </q-item-section>
           </q-item>
 
@@ -26,8 +27,11 @@
             </q-item-section>
             <q-item-section>
               <q-item-label overline> Wo </q-item-label>
-              <q-item-label>LifeLine Fitnessstudio, <span class="text-weight-bold">Albert-Schweizer-Gasse 6</span></q-item-label>
-              <q-item-label caption>1140 Wien</q-item-label>
+              <q-item-label> {{ workshop.location.name }}, <span class="text-weight-bold">
+                  {{ workshop.location.address }}
+                  {{ workshop.location.houseNumber }}
+                </span></q-item-label>
+              <q-item-label caption> {{ workshop.location.zip }} {{ workshop.location.city }} </q-item-label>
             </q-item-section>
           </q-item>
 
@@ -37,7 +41,7 @@
             </q-item-section>
             <q-item-section>
               <q-item-label overline>Preis</q-item-label>
-              <q-item-label><span class="text-weight-bold">€35,--</span> pro Person</q-item-label>
+              <q-item-label><span class="text-weight-bold">€{{ workshop.price }}</span> pro Person</q-item-label>
             </q-item-section>
           </q-item>
 
@@ -47,16 +51,16 @@
             </q-item-section>
             <q-item-section>
               <q-item-label overline>Anmeldung</q-item-label>
-              <q-item-label><span class="text-weight-bold"> office@jsproject.at </span></q-item-label>
+              <q-item-label><span class="text-weight-bold"> {{ workshop.signUp }} </span></q-item-label>
             </q-item-section>
           </q-item>
 
-          <q-item>
+          <q-item v-for="(feature, key) in workshop.features" :key=key>
             <q-item-section avatar>
               <q-icon color="secondary" name="arrow_forward" />
             </q-item-section>
             <q-item-section>
-              <q-item-label>inkl. <span class="text-weight-bold"> Q&A </span></q-item-label>
+              <q-item-label> {{ feature }} </q-item-label>
             </q-item-section>
           </q-item>
         </q-list>
@@ -66,28 +70,40 @@
       <div class="lt-md col-12 q-my-md"></div>
 
       <div class="col-12 col-md-6">
-        <q-img @click="() => (bigImagePopUp = true)" src="/images/posters/WS_0612_2025_quadratisch.png" class="poster" />
+        <q-img @click="() => (bigImagePopUp = true)" :src="workshop.image.square" class="poster" />
       </div>
-    </div>
 
-    <q-dialog v-model="bigImagePopUp" backdrop-filter="blur(4px)" maximized>
-      <q-card>
-        <q-bar>
-          <q-space />
-          <q-btn dense flat icon="close" v-close-popup>
-            <q-tooltip class="bg-white text-primary">Close</q-tooltip>
-          </q-btn>
-        </q-bar>
-        <q-img @click="() => (bigImagePopUp = false)" src="/public/images/posters/WS_0612_2025.png" />
-      </q-card>
-    </q-dialog>
+      <q-dialog v-model="bigImagePopUp" backdrop-filter="blur(4px)" maximized>
+        <q-card>
+          <q-bar>
+            <q-space />
+            <q-btn dense flat icon="close" v-close-popup>
+              <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+            </q-btn>
+          </q-bar>
+          <q-img @click="() => (bigImagePopUp = false)" :src="workshop.image.regular" />
+        </q-card>
+      </q-dialog>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Ref } from 'vue'
 import { ref } from 'vue'
+import { format } from 'date-fns'
+
+import type { Workshop } from '@/types'
+import { parseWorkshops } from '@/helpers'
+
+import workshopData from '@/assets/data/workshops.json'
 
 const bigImagePopUp = ref(false)
+const workshops: Ref<Workshop[]> = ref(parseWorkshops(workshopData))
+
+// Sort the workshops descending by their date
+workshops.value.sort((lhs, rhs) => { return Number(lhs.begin) - Number(rhs.begin) }).reverse()
+console.log(workshops.value[0].image)
 </script>
 
 <style lang="scss" scoped>
